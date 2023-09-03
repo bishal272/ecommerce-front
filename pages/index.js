953e576489d50ed8f321797d0f89dd66550cff07
bishal272 <merkasin272@gmail.com>
2023-09-04 03:24:12 +0530

    random featured product fixed

--- pages/index.js
+++ pages/index.js
@@ -17,11 +17,11 @@ export default function index({ featuredProduct, newProducts }) {
   );
 }
 export async function getServerSideProps() {
-  // const ids = (await Product.find({})).map((p) => p._id.toString());
-  // const heroProduct = ids[Math.floor(Math.random() * ids.length)];
-  const heroProduct = "64f202acccdea741adeeffaf";
   await mongooseConnect();
-  const featuredProduct = await Product.findById(heroProduct);
+  // const heroProduct = "64f202acccdea741adeeffaf";
+  const ids = await Product.find({});
+  const idsArr = ids.map((i) => i._id.toString());
+  const featuredProduct = await Product.findById(idsArr[Math.floor(Math.random() * idsArr.length)]);
   const newProducts = await Product.find({}, null, { sort: { _id: -1 }, limit: 10 });
 
   return {
